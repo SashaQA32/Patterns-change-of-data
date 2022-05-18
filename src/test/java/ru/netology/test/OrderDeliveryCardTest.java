9e2a431afe659e4ae21fe49270766a5bd6f18162
Александр Стрюков <sanyajump16@gmail.com>
2022-05-18 22:13:57 +0300

    Added package ru.netology.data

--- src/test/java/ru/netology/test/OrderDeliveryCardTest.java
+++ src/test/java/ru/netology/test/OrderDeliveryCardTest.java
@@ -4,14 +4,14 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import ru.netology.GeneratorDataCardOrder;
+import ru.netology.data.GeneratorDataCardOrder;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static ru.netology.GeneratorDataCardOrder.getUserInfo;
+import static ru.netology.data.GeneratorDataCardOrder.getUserInfo;
 
 public class OrderDeliveryCardTest {
     GeneratorDataCardOrder.UserInfo user = getUserInfo();
